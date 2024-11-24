a878971b167d3721f185020d4cb46b1a62dfc157
павел коренистов <korenistov.pasha@ya.ru>
2024-11-24 16:36:49 +0500

    test

---
+++ TestVoid/TestVoid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVoid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (float f = 10; float.IsInfinity(f); f *= f)
+            {
+                Console.WriteLine(f);
+            }
+            for (double f = 10; double.IsInfinity(f); f *= f)
+            {
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
